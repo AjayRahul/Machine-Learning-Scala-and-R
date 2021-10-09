b42805531f5f8980461f07f8cc731592f15e801f
Ajay Rahul <31838370+AjayRahul@users.noreply.github.com>
2021-10-09 19:39:47 +0530

    Update and rename T.scala to TitianicKaggle.scala

--- TitianicKaggle.scala
+++ TitianicKaggle.scala
@@ -1,4 +1,4 @@
-package com.sundogsoftware.spark
+
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.feature.{OneHotEncoder, StringIndexer, VectorAssembler, VectorIndexer}
@@ -6,30 +6,40 @@ import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.mllib.evaluation.MulticlassMetrics
 import org.apache.spark.sql.functions.col
 
-object T {
-  def main(args: Array[String]):Unit= {
+object TitanicKaggle {
+  def main(args: Array[String]):Uni t= {
+  
+    // creatring spark session
+    
     val spark = SparkSession
       .builder()
       .appName("mlPratice")
       .master("local[*]")
       .getOrCreate()
 
-    // COMMAND ----------
+    // importing the data into the spark session
 
     val df = spark.read.option("header", "true")
       .option("inferschema", "true")
       .format("csv")
-      .load("C:\\Users\\Lenovo\\Documents\\SparkScalaCourse\\SparkScalaCourse\\IdeaProjects\\Ttitanic\\titanic_train.csv")
+      .load("/titanic_train.csv")
 
     df.show(10)
     df.printSchema()
+    
+    // first view to check for the sample data
 
     df.createOrReplaceTempView("d1")
-    val sd = spark.sql("SELECT survived as label, Pclass, Name, Sex, Age, SibSp, Parch, Fare, Embarked FROM d1")
-
+   
+    val sd = spark.sql("SELECT survived, Pclass, Name, Sex, Age, SibSp, Parch, Fare, Embarked FROM d1")
+    
+    // drop  the null values
+    
     val nd = sd.na.drop()
     nd.show(10)
-
+    
+    // coverting the Sex and Emabarked columns to int to process
+    
     val x = new StringIndexer().setInputCol("Sex").setOutputCol("SexVector")
     val y = new StringIndexer().setInputCol("Embarked").setOutputCol("EmbarkedVector")
 
@@ -37,7 +47,7 @@ object T {
     val y1 = new OneHotEncoder().setInputCol("EmbarkedVector").setOutputCol("EmbarkedChanged")
 
     val assembler = (new VectorAssembler()
-      .setInputCols(Array("Pclass", "SexChanged", "EmbarkedChanged", "Age", "SibSp", "Parch", "Fare"))
+      .setInputCols(Array("survived", "SexChanged", "EmbarkedChanged", "Age", "SibSp", "Parch", "Fare"))
       .setOutputCol("features"))
 
     val Array(training, test) = nd.randomSplit(Array(0.7, 0.3), seed = 55)
@@ -45,7 +55,9 @@ object T {
     import org.apache.spark.ml.Pipeline
 
     val lr = new LogisticRegression()
-
+    
+    // putting all the encoders and the assemblers in a pipeline
+    
     val pipeline = new Pipeline().setStages(Array(x, y, x1, y1, assembler, lr))
 
     val model = pipeline.fit(training)
@@ -55,7 +67,9 @@ object T {
     res.show(10)
 
     res.printSchema()
-
+    
+    // exporting the df to a json file to process in RStudio for visualization
+    
     res.coalesce(1).write.format("json").save("titanicMlResult")
 
   }
