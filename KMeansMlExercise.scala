1e42c7835f9ab3345c8c44fb043322278e878098
Ajay Rahul <21rahulajay@gmail.com>
2021-10-26 10:47:22 +0530

    Create KMeansMlExercise.scala

---
+++ KMeansMlExercise.scala
@@ -0,0 +1,18 @@
+package com.sundogsoftware.spark
+import org.apache.spark.sql.SparkSession
+import org.apache.log4j._
+
+object KMeansMlExercise {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel("Level.ERROR")
+
+    val spark = SparkSession.builder().master("local[*]").getOrCreate()
+
+    val df = spark.read.format("csv").load("C:/Users/Lenovo/Desktop/Udemy/Scala ML/Wholesale customers data.csv")
+
+    val kmeans = new Kmeans().setK(2).setSeed(1L)
+    val model = kmeans.fit(df)
+    val WESS = model.computeCost(df)
+    model.clusterCenter.foreach(println)
+  }
+}
